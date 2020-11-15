8c1d654c1af4f23004dc41fbb28693f7db1bbefc
Jonathan Martin <marti243@coventry.ac.uk>
2020-11-15 14:32:45 +0000

    first commit

---
+++ modules/files.js
@@ -0,0 +1,25 @@
+import sqlite from 'sqlite-async'
+class Files{
+  constructor(dbName =':memory:'){
+    return(async()=>{
+      this.db=await sqlite.open(dbName)
+      const sql='CREATE TABLE IF NOT EXISTS homepage(\
+        id INTEGER PRIMARY KEY AUTOINCREMENT,\
+        userid INTEGER,\
+        uploadname TEXT NOT NULL,\
+        filetype TEXT NOT NULL,\
+        FOREIGN KEY(userid) REFERENCES users(id)\
+      );'
+      await this.db.run(sql)
+      return this
+    })()
+  }
+  async all() {
+    const sql='SELECT users.user, homepage.* FROM homepage, users\
+      WHERE homepage.userid = users.id'
+    const files = await this.db.all(sql)
+    return files
+  }
+  
+ }
+export default Files 
---
+++ routes/secure.js
@@ -0,0 +1,29 @@
+
+import Router from 'koa-router'
+import Files from '../modules/files.js'
+const dbName ='website.db'
+const router = new Router({ prefix: '/secure' })
+
+async function checkAuth(ctx, next) {
+	console.log('secure router middleware')
+	console.log(ctx.hbs)
+	if(ctx.hbs.authorised !== true) return ctx.redirect('/login?msg=you need to log in&referrer=/secure')
+	await next()
+}
+
+router.use(checkAuth)
+
+router.get('/', async ctx => {
+  const files=await new Files(dbName)
+	try {
+    const records =await files.all()
+    console.log(records)
+    ctx.hbs.records=records
+		await ctx.render('secure', ctx.hbs)
+	} catch(err) {
+		ctx.hbs.error = err.message
+		await ctx.render('error', ctx.hbs)
+	}
+})
+
+export default router
