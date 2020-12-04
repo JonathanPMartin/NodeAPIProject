f78ffc98842a763b5bf7d38d58e678db72576f7b
Jonathan Martin <marti243@coventry.ac.uk>
2020-12-04 17:12:56 +0000

    removed all linter issues

--- routes/homepage.js
+++ routes/homepage.js
@@ -3,30 +3,30 @@ import Router from 'koa-router'
 import Files from '../modules/files.js'
 const dbName ='website.db'
 const router = new Router({ prefix: '/homepage' })
+function dataval(ctx) {
+	const data={
+		uid: ctx.hbs.userid,
+		uploadname: ctx.hbs.filename,
+		filetype: ctx.hbs.filetype,
+		file: ctx.hbs.file,
+		filesize: ctx.hbs.filesize,
+		des: ctx.hbs.des
+	}
+	return data
+}
 async function checkAuth(ctx, next) {
-	console.log('secure router middleware')
-	console.log(ctx.hbs.userid)
+	const files= await new Files(dbName)
 	if(ctx.hbs.authorised !== true) {
 		return ctx.redirect(`/login?msg=you need to log in&referrer=/homepage?userid=${ctx.hbs.userid}`)
 	}
 	if(ctx.hbs.userid === undefined) return ctx.redirect('/logout')
 	if (ctx.hbs.delete !== undefined) {
-		const files= await new Files(dbName)
 		await files.delete(ctx.hbs.column)
 		await files.close()
 		return ctx.redirect(`/homepage?userid=${ctx.hbs.userid}`)
 	}
 	if(ctx.hbs.des !== undefined) {
-		const data={
-			uid: ctx.hbs.userid,
-			uploadname: ctx.hbs.filename,
-			filetype: ctx.hbs.filetype,
-			file: ctx.hbs.file,
-			filesize: ctx.hbs.filesize,
-			des: ctx.hbs.des
-		}
-		const files=await new Files(dbName)
-		await files.add(data)
+		await files.add(dataval(ctx))
 		await files.close()
 		return ctx.redirect(`/homepage?userid=${ctx.hbs.userid}`)
 	}
