18371991ceac93df95b44690535de9400298cec6
Jonathan Martin <marti243@coventry.ac.uk>
2020-12-10 16:01:47 +0000

    fixed linter warning from last commit

--- routes/details.js
+++ routes/details.js
@@ -4,8 +4,8 @@ const dbName ='website.db'
 const router = new Router({ prefix: '/details' })
 router.get('/', async ctx => {
 	const files=await new Files(dbName)
-	try { 
-		const record=await files.row(ctx.hbs.row)<!--fetchs the row data on the database that the file is on -->
+	try {
+		const record=await files.row(ctx.hbs.row)//fetchs the row data on the database that the file is on
 		console.log(record[0])
 		ctx.hbs.record=record[0] //stores record in a manner the handlebars can understand
 		await ctx.render('details', ctx.hbs)
